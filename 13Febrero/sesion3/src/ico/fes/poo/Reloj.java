2c9da223f7c8dc2dcfe2e9c4ac867d8d11f00dd3
amairanii1608 <60655527+amairanii1608@users.noreply.github.com>
2020-02-21 00:01:20 -0600

    Update Reloj.java

--- 13Febrero/sesion3/src/ico/fes/poo/Reloj.java
+++ 13Febrero/sesion3/src/ico/fes/poo/Reloj.java
@@ -1,42 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ico.fes.poo;
 
-/**
- *
- * @author LABCOM2
- */
+
 public class Reloj {
     private int hora;
     private int minuto;
     private int segundo;
+    private boolean contador=true;
 
-    public Reloj() {
-    }
-
+  
+  
     public Reloj(int hora, int minuto, int segundo) {
         this.hora = hora;
         this.minuto = minuto;
         this.segundo = segundo;
     }
 
-    public int getSegundo() {
-        return segundo;
+    public in getHora() {
+        return hora;
     }
+    
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+    
+    public in getSegundo(int segundo) {
+        return segundo;
+    }   
 
     public void setSegundo(int segundo) {
         this.segundo = segundo;
-    }
-
-    public int getHora() {
-        return hora;
-    }
-
-    public void setHora(int hora) {
-        this.hora = hora;
     }
 
     public int getMinuto() {
@@ -49,6 +42,8 @@ public class Reloj {
     
     public void mostrarHora(){
         System.out.println(this.hora + ":"+this.minuto + ":"+this.segundo);
+        
+        while(contador=true){
     }
     
 }
